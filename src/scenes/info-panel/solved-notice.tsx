6494bc413d74be62cedec4c9eb6a22aba822f9fb
Tom Yancey <2228796+thyancey@users.noreply.github.com>
2022-11-26 21:48:30 -0500

    markdown support for hints, make solved notice more subtle (#12)

--- src/scenes/info-panel/solved-notice.tsx
+++ src/scenes/info-panel/solved-notice.tsx
@@ -16,7 +16,7 @@ const slideInFromBottom = keyframes`
 `;
 const fadeOut = keyframes`
   0% {
-    opacity: .5;
+    opacity: 1;
   }
   100% {
     opacity: 0;
@@ -27,37 +27,36 @@ const StyledSolvedNotice = styled.div`
   grid-column: 1 / span 2;
   grid-row: 3 / span 1;
   position: relative;
-  animation: ${fadeOut} .3s ease-in 1s;
+  animation: ${fadeOut} .25s ease-out .5s;
   animation-fill-mode: backwards;
   opacity: 0;
 
   div{
-    animation: ${slideInFromBottom} .3s cubic-bezier(.56,1.85,.74,.63) 0s 1;
+    animation: ${slideInFromBottom} .2s cubic-bezier(.56,1.85,.74,.63) 0s 1;
     position:absolute;
-    width:100%;
     bottom:100%;
     opacity: 1;
     text-align:right;
-    padding-right: .5rem;
-    padding-bottom: 1rem;
+    padding: .5rem 1rem;
+    right: .5rem;
 
     span{
-      font-size:3rem;
+      font-size:2rem;
     }
   }
 `;
 
 const StyledCorrectNotice = styled(StyledSolvedNotice)`
   div{
-    box-shadow:0 -2px 1rem 2rem ${getColor('green')};
+    /* box-shadow:0 -2px 1rem 2rem ${getColor('green')}; */
     background-color: ${getColor('green')};
   }
 `;
 
 const StyledIncorrectNotice = styled(StyledSolvedNotice)`
   div{
-    box-shadow:0 -2px 2rem 2rem ${getColor('red')};
-    background-color: ${getColor('red')};
+    /* box-shadow:0 -2px 2rem 2rem ${getColor('red_light')}; */
+    background-color: ${getColor('red_light')};
   }
 `;
 
